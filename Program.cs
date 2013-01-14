59039fba0f98528016caf5b56b6cb9ec10dc2370
Stephen Trusheim <stephen@trushe.im>
2013-01-13 18:32:40 -0800

    Updating copyright

--- Program.cs
+++ Program.cs
@@ -1,21 +1,13 @@
 //--------------------------------------------------------------------
-// FILENAME: Program.cs
+// Stanford LiveEvent / LiveID scanner technology
 //
-// Copyright(c) 2009 Symbol Technologies Inc. All rights reserved.
+// Copyright (c) 2011-2013 Stanford University
+// Primary developer: Stephen Trusheim 
+//        (trusheim@stanford.edu / stephen@trushe.im)
+// See README for licensing information
 //
-// DESCRIPTION:
-//      This module is part of the ScanInventory application.   It provides
-//      the entry point for the application.  
-//
-// NOTES:
-//      Prior to loading the MainForm, the entry point looks to see if 
-//      it connect to MT2000 Scanner services and place it in the correct 
-//      mode.  If this is completes without error, the main form is loaded. 
-//
-//      This software is provided as is as an example of how to use the 
-//      MT2000 Scanner services assemblies. 
-//      
-// 
+// Based on Symbol Technologies ScanInventory sample application
+// copyright (c) 2009 Symbol Technologies, Inc.
 //--------------------------------------------------------------------
 using System;
 using System.Windows.Forms;
