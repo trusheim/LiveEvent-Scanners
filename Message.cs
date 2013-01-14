38d11e0d161b26045ac1a933ece6bf965bb0bd34
Stephen Trusheim <stephen@trushe.im>
2013-01-13 16:29:40 -0800

    Continuation of 364a85b for variable defined messages

---
+++ Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace SU_MT2000_SUIDScanner
+{
+    class Message
+    {
+        public short id = 0;
+        public string big_message = "";
+        public string small_message = "";
+
+        public Message(short id, string big_message, string small_message)
+        {
+            this.id = id;
+            this.big_message = big_message;
+            this.small_message = small_message;
+        }
+
+    }
+
+    class Messages : SortedList<short, Message> { }
+}
